5625da56ce173ba3e76f25606adfec3de9fa2a15
Jake Mcneill <jake-cryptic@users.noreply.github.com>
2023-03-27 01:28:54 +0100

    Add new challenge files

---
+++ rem-ncserver/run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+docker build -t rctf_rem_ncserver .
+
+#docker rm rctf_ncserver_app
+
+docker run \
+    -p 5555:5555 \
+    --name rctf_ncserver_app \
+    rctf_rem_ncserver
---
+++ rem-ncserver/server.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+while true
+do
+  echo nc starting
+  ncat -nvl -p 5555 -e /bin/bash
+done
