2e64e06870ecdf38c74418b4dfca44388fd40a71
Jake Mcneill <jake-cryptic@users.noreply.github.com>
2023-03-31 17:17:04 +0100

    [Rem:NCServer] Fixes

--- rem-ncserver/run.sh
+++ rem-ncserver/run.sh
@@ -5,6 +5,6 @@ docker build -t rctf_rem_ncserver .
 docker container rm rctf_ncserver_app
 
 docker run \
-    -p 5555:5555 \
-    --name rctf_ncserver_app \
-    rctf_rem_ncserver
+  -p 5555:5555 \
+  --name rctf_ncserver_app \
+  rctf_rem_ncserver
--- rem-ncserver/server.sh
+++ rem-ncserver/server.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-while true
-do
+while true; do
   echo nc starting
   ncat -nvl -p 5555 -e /bin/bash
 done
