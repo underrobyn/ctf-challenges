32efb70323b639cb63a861b20a82cb3f056bcf25
Jake Mcneill <jake-cryptic@users.noreply.github.com>
2023-04-17 04:23:58 +0100

    Renamed inf-lookclosely to int-

---
+++ int-lookclosely/README.md
@@ -0,0 +1,4 @@
+# Int-LookClosely
+
+Server header: flag{1nf0rm4t10n_15_v41u4b13}
+
--- int-lookclosely/run.sh
+++ int-lookclosely/run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-docker build -t rctf_inf_lookclosely .
+docker build -t rctf_int_lookclosely .
 
 docker run \
     -p 8080:8080 \
     --name rctf_lookclosely_app \
-    rctf_inf_lookclosely
+    rctf_int_lookclosely
