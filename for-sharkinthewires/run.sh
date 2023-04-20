d03f4487b31d5b8464e08951864616619b0926fc
Jake Mcneill <jake-cryptic@users.noreply.github.com>
2023-04-20 01:50:56 +0100

    Added pcap files for forensics challenges

--- for-sharkinthewires/run.sh
+++ for-sharkinthewires/run.sh
@@ -6,5 +6,6 @@ docker container rm rctf_sharkinthewires_app
 
 docker run \
     -p 23:23 \
+    --hostname jellyfish-srv221.internal.clam-corp.com \
     --name rctf_sharkinthewires_app \
     rctf_for_sharkinthewires
